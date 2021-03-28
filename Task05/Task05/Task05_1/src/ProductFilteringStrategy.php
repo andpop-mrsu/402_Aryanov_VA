b027ff7d75523b53bb9140d74d24bf7c6134b3d1
vladislavAA <aryanov.vladislav@gmail.com>
2021-03-28 08:52:27 +0300

    Update

--- Task05/Task05/Task05_1/src/ProductFilteringStrategy.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App;
-
-interface ProductFilteringStrategy
-{
-    public function filter(array $collection);
-}
--- Task05/Task05/Task05_3/src/HotelRoom.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App;
-
-interface HotelRoom
-{
-    public function getDescription();
-    public function getPrice();
-}
